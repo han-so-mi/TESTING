4757a522303faa92d6c6926263b92a250f3f321d
HAN-SO-MI <miso74710@gmail.com>
2020-06-11 16:15:26 +0530

    ASSIGNMENT-LEXCIAL SCOPING

---
+++ cachematrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+
+makeCacheMatrix <- function(x = matrix()) {  
+  inv <-NULL
+  set <-function(y){
+    x <<-y
+    inv <<-NULL
+  }
+  get <-function(){x}
+  SetInverse <-function (inverse) { inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set =set , get=get , SetInverse=SetInverse,getInverse=getInverse)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <-x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$SetInverse(inv)
+  inv
+}
